a5e481ad6503f2c6da570bd2c3df5fddb6301e98
UmbertoSerino <serumberto@gmail.com>
2024-01-15 12:27:58 +0100

    inserted foreach

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-$match = [
-    'Club_1' => 'Messina', 'Club_2' => 'Milano', 'punti_1' => 85, 'punti_2' => 90
+$matchs = [
+    'Club_1' => 'Messina', 'Club_2' => 'Milano', 'punti_1' => 85, 'punti_2' => 90,
+    'Club_3' => 'Avellino', 'Club_4' => 'Ancona', 'punti_3' => 60, 'punti_4' => 101
 ];
+
+foreach ($matchs as $matche) {
+    echo ' partita: ' . $matche;
+};
